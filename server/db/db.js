ecee7d4bd58605663ad4904d947b2f2d4e5f2eb3
Matt Alexander <dmalexander6@gmail.com>
2023-01-23 12:35:08 -0700

    install mongoose and initial file setup

---
+++ server/db/db.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+// const ObjectId = mongoose.Types.ObjectId;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority/${process.env.DB_NAME}`;
+const db = mongoose.connect(uri).catch(err => console.log('ERROR CONNECTIONG TO DB...', err));
+const { Schema, model } = mongoose;
+
+const userStatics = require('./statics/user');
+const sessionStatics = require('./statics/session');
+
+/////////////////////////////////////////////////////////////////
+
+// Define the schemas
+const UserSchema = new Schema({
+  // TODO
+}, {
+  versionKey: false,
+  toJSON: { virtuals: true }, // So `res.json()` and other `JSON.stringify()` functions include virtuals
+  toObject: { virtuals: true } // So `console.log()` and other functions that use `toObject()` include virtuals
+});
+
+const SessionSchema = new Schema({
+  // TODO
+}, {
+  versionKey: false,
+  toJSON: { virtuals: true }, // So `res.json()` and other `JSON.stringify()` functions include virtuals
+  toObject: { virtuals: true } // So `console.log()` and other functions that use `toObject()` include virtuals
+});
+
+// Define the virtuals
+  // TODO
+
+// Define the static methods
+UserSchema.statics = userStatics;
+SessionnSchema.statics = sessionStatics;
+
+// Compile the schemas
+const User = model('User', UserSchema);
+const Session = model('Session', SessionSchema);
+
+User.createCollection();
+Session.createCollection();
+
+//////////////////////////////////////////////////
+
+const disconnect = function () {
+  return mongoose.connection.close();
+}
+
+//////////////////////////////////////////////////
+
+module.exports = { User, Session, disconnect };
