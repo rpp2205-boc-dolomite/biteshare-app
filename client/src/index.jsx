ab65b3d3cd59e6afdc0236353f204791b06a6aee
Rain <pyc0422@gmail.com>
2023-01-23 19:52:19 -0600

    conflig result

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import App from './components/App.jsx';
-<<<<<<< HEAD
-import AddFriends from './components/createMeal/AddFriends.jsx';
-=======
 import Navbar from './components/Navbar.jsx';
 
->>>>>>> main
 
 import {
   createBrowserRouter,
@@ -15,15 +11,9 @@ import {
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>},
-<<<<<<< HEAD
-  // {path:"/addfriends", element:<AddFriends />}
-  // {path:"/signup", element:<SignUp />}
-  // {path:"/login", element:<LogIn />}
-=======
   // {path:"/signup", element:<SignUp />},
   //{path:"/login", element:<LogIn />},
   {path:"/dashboard", element:<Navbar />}
->>>>>>> main
 ])
 
 createRoot(document.getElementById('app')).render(
