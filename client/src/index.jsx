1b599b27037e3ebbc22564354cf1d5ae2ff60471
Rain <pyc0422@gmail.com>
2023-01-24 14:35:56 -0600

    add comma

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
   {path:"/addfriends", element:<AddFriends />},
   {path:"/signup", element:<SignUp />},
   {path:"/login", element:<Login />},
-  {path:"/dashboard", element:<Navbar />}
+  {path:"/dashboard", element:<Navbar />},
   {path:"/meals", element:<Meals />}
 ])
 
