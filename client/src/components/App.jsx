fe4dfa5b42cdf445fee5dfc1a352f27fe7a67cfd
Rain <pyc0422@gmail.com>
2023-01-23 19:54:14 -0600

    delete loggedin file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import HomePage from './HomePage.jsx';
-import LoggedIn from './LoggedIn.jsx';
 
 const App = (props) => {
 
   //we may need to render different page after login. so i created a simple app components
   return (
     <HomePage />
-    // <Header />
   )
 }
 
