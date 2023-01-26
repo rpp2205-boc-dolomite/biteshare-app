1f5f2d3eb245b33c96c99ff9a97a821f64c16e98
Matt Alexander <dmalexander6@gmail.com>
2023-01-25 20:08:05 -0700

    make getUser work with phone numbers too

--- server/app.js
+++ server/app.js
@@ -32,12 +32,12 @@ app.use(express.urlencoded({ extended: false }));
 
 
 //---  user's friends list ---//
-app.get('/api/friends', friendsControllers.getFriends)
-app.post('/api/friends', friendsControllers.addFriends)
+app.get('/api/friends', friendsControllers.getFriends);
+app.post('/api/friends', friendsControllers.addFriends);
 
 //---- user info ---//
-app.get('/api/users/:user_id', userControllers.getUser)
-app.post('/api/users', userControllers.addUser)
+app.get('/api/users/', userControllers.getUser);
+app.post('/api/users', userControllers.addUser);
 
 
 app.get('*', (req, res) => {
