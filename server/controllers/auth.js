2fe06144d287b5ed5c94d85104495a9c70908d16
Matt Alexander <dmalexander6@gmail.com>
2023-01-28 10:43:25 -0700

    add phone number parsing to verifyLogin controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const db = require('../db/db.js');
 const bcrypt = require('bcrypt');
+const parsePhoneNumber = require('libphonenumber-js');
 
 exports.createHash = function(password) {
     var salt = bcrypt.genSaltSync(8)
@@ -9,7 +10,12 @@ exports.createHash = function(password) {
 };
 
 exports.verifyLogin = function(req, res) {
-    db.User.findOne({phone_num: req.body.phone_num})
+    const phoneNumber = parsePhoneNumber(req.body.phone_num, 'US');
+    if (!phoneNumber) {
+        res.status(500).send("Connot parse phone number");
+        return;
+    }
+    db.User.findOne({phone_num: phoneNumber})
     .then((user) => {
         console.log('user from DB', user)
         if (user !== null) {
