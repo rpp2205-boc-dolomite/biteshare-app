18a9424b6f78185daafc2bb9036bc963fe30642c
Matt Alexander <dmalexander6@gmail.com>
2023-01-28 16:25:15 -0700

    remove dummy data

--- client/src/components/MealDetails/MealDetails.jsx
+++ client/src/components/MealDetails/MealDetails.jsx
@@ -29,25 +29,27 @@ import {
 
 import { Link, useLocation, Navigate } from 'react-router-dom';
 
-const restName = "Red Robin";
+// const restName = "Red Robin";
 
-const passedInData = {
-  host: { user_id: '', name: "Ellen", phone_num: "" },
-  friends: [{ name: "Clark W. Griswold", phone_num: "+12223334444" }, { name: "Cousin Eddie", phone_num: "+12123234343" }],
-  resInfo: { name: "Red Robin", address: "" },
-};
+// const state = {
+//   host: { user_id: '', name: "Ellen", phone_num: "" },
+//   friends: [{ name: "Clark W. Griswold", phone_num: "+12223334444" }, { name: "Cousin Eddie", phone_num: "+12123234343" }],
+//   restInfo: { name: "Red Robin", address: "" },
+// };
 
 const setFriendData = function (index, change) {
   if (index === -1) {
-    Object.assign(passedInData.host, change);
+    Object.assign(state.host, change);
   } else {
-    Object.assign(passedInData.friends[index], change);
+    Object.assign(state.friends[index], change);
   }
 };
 
 export default function MealDetails(props) {
+  const { state } = useLocation();
 
-  const [guests, setGuests] = React.useState(passedInData.friends.length + 1);
+  if (!state.friends || !state.host || !state.restInfo) { console.warn('MealDetails is missing some data')}
+  const [guests, setGuests] = React.useState(state.friends.length + 1);
   const [splitMethod, setSplitMethod] = React.useState('even');
   const [mealTotal, setMealTotal] = React.useState(0);
   const [evenMealAmt, setEvenMealAmt] = React.useState(0);
@@ -57,8 +59,7 @@ export default function MealDetails(props) {
   const [session, setSession] = React.useState({});
   const [redirect, setRedirect] = React.useState(false);
 
-  const { state } = useLocation();
-  console.log('mealdetail', {state});
+  console.log('mealdetail', state);
   const handleMealTotalChange = function (value) {
     setMealTotal(value);
     setEvenMealAmt(value / guests);
@@ -75,6 +76,7 @@ export default function MealDetails(props) {
 
     if (session) {
       console.log('Session is valid! Navigating to review page');
+      console.log('SESSION', session);
       setRedirect(true);
     } else {
       // Don't submit
@@ -85,12 +87,12 @@ export default function MealDetails(props) {
   const createAndValidateSession = function () {
     const newSession = {
       host: {
-        ...passedInData.host,
+        ...state.host,
         meal_amount: hostMealAndTip[0],
         tip_amount: hostMealAndTip[1],
       },
-      friends: passedInData.friends,
-      rest_name: passedInData.resInfo.name,
+      friends: state.friends,
+      rest_name: state.restInfo.name,
       sub_total: mealTotal,
       tip_total: mealTotal * tipPercent,
       receipt: receipt,
@@ -110,7 +112,7 @@ export default function MealDetails(props) {
 
   return (<>
     <FormLabel>Restaurant:</FormLabel>
-    <Typography>{restName}</Typography>
+    <Typography>{state && state.restInfo && state.restInfo.name}</Typography>
 
     <OutlinedInput
       id="bill-amount-text-field"
@@ -170,7 +172,7 @@ export default function MealDetails(props) {
       <ToggleButton value="custom">Custom</ToggleButton>
     </ToggleButtonGroup>
 
-    <CustomSplit hidden={splitMethod === 'even'} {...{ setFriendData, mealTotal, evenMealAmt, ...passedInData }} />
+    <CustomSplit hidden={splitMethod === 'even'} {...{ setFriendData, mealTotal, evenMealAmt, ...state }} />
 
     <Button
       onClick={handleSubmit}
