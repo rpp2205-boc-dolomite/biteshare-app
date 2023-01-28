01a8e32933f800295a123f375ec92defa7aff7f1
Matt Alexander <dmalexander6@gmail.com>
2023-01-28 01:27:23 -0700

    add session validation and fix headers

--- client/src/components/MealDetails/ReceiptUpload.jsx
+++ client/src/components/MealDetails/ReceiptUpload.jsx
@@ -14,18 +14,15 @@ const uploader = Uploader({
 const options = { multi: false }
 
 // Render the file upload button:
-const ReceiptUpload = () =>
+const ReceiptUpload = ({ setReceipt }) =>
   <UploadButton uploader={uploader}         // Required.
-                options={options}           // Optional.
-                onComplete={files => {      // Optional.
-                  if (files.length === 0) {
-                    console.log('No files selected.')
-                  } else {
-                    console.log('Files uploaded:');
-                    console.log(files.map(f => f.fileUrl));
-                  }
-                }}>
-    {({onClick}) =>
+    options={options}           // Optional.
+    onComplete={files => {      // Optional.
+      if (files.length !== 0) {
+        setReceipt(files[0]);
+      }
+    }}>
+    {({ onClick }) =>
       <Button onClick={onClick}>
         Upload a file...
       </Button>
