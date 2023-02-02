b3590f76bdc9e05e56d6eaddcab741bdc4caed34
hray <101483930+bubsinthemountains@users.noreply.github.com>
2023-02-02 18:32:09 -0500

    more work

--- client/src/components/Dashboard/ReactionsComment.jsx
+++ client/src/components/Dashboard/ReactionsComment.jsx
@@ -1,22 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import {Button, Box, Typography, Stack, List, ListItem, ListItemButton} from '@mui/material';
-import data from '@emoji-mart/data'
-import Picker from '@emoji-mart/react'
+
 
 const ReactionsComment = ({}) => {
 
 
   return (
     <Box>
-      <div className="reactions">
-        <Picker
-          data={data}
-          onEmojiSelect={console.log}
-          showPreview={false}
-          showSkinTones={false}
+      <Typography
 
-          />
-      </div>
+      >
+        👍 ♥️ 🔥 🦷 | 💬
+      </Typography>
     </Box>
   )
 }
