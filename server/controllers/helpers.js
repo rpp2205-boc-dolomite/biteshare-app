f9a006ca2ce16fe1a625648429e7ef1bf36d55d4
Stacey Pereira <staceypereira1@gmail.com>
2023-02-02 20:24:57 -0800

    review page updated and post sessions complete

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,23 +1,34 @@
 require('dotenv').config();
 
 
-exports.sendTexts = async function (array) {
+exports.sendTexts = async function (input) {
   // this is an async function and returns an array of results
   // Accepts an array of these arrays: ['+12223334444', "Hello! This is a text from BiteShare!"]
   // https://www.twilio.com/docs/sms/api
 
-  if (!Array.isArray(array) || !array.length) { return }
+  if (!Array.isArray(input.friends) || !input.friends.length) { return }
 
   const from = process.env.TWILIO_PHONE_NUM;
   const client = require("twilio")(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-  const promises = [];
+  //const promises = [];
 
-  for (const [to, body] of array) {
-    promises.push(client.messages
-      .create({ body, from, to })
-      .then(message => message.error_code ? message.error_code : message.sid));
-  }
+  const promises = input.friends.map((element) => {
+    return(
+      client.messages.create({
+        body: `Hi ${element.name}: \n You have been invited to split the bill at ${input.restInfo.name} \n Please select the link below to view the meal session \n www.google.com`,
+        from: from,
+        to: `${element.phone_num}`
+      })
+      .then(message => console.log(message.sid))
+    )
+  })
+
+  // for (const [to, body] of array) {
+  //   promises.push(client.messages
+  //     .create({ body, from, to })
+  //     .then(message => message.error_code ? message.error_code : message.sid));
+  // }
 
   return Promise.all(promises);
 };
