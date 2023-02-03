4a2990bad0e6d225c1519fec1f69933cf93e7064
Zachary Atha <zacharyatha@gmail.com>
2023-02-03 17:03:05 -0500

    Created testing environment

---
+++ client/src/components/Login.test.js
@@ -0,0 +1,8 @@
+import { render, screen, waitFor } from '@testing-library/react'
+
+import '@testing-library/jest-dom';
+import SignIn from './Login.jsx';
+
+test('test runs', async () => {
+    expect(1+1).toEqual(2)
+})
