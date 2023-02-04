f7b00342903f7bcf67109131eff9bfe8e8c8cbe3
Stacey Pereira <staceypereira1@gmail.com>
2023-02-03 19:24:39 -0800

    mealsList fixed, testing Twilio function

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -22,7 +22,39 @@ exports.getSessions = function(req, res) {
 }
 
 exports.postSessions = function(req, res) {
+  //in req.body is the info
+  // user addUser controller to add new users
+  // returns an insertedIds array which I will use to be the keys
+    // for details field
+  //
 
+  //var comment = {[user_id]: {comment: 'I wanna go here', date: timestamp}}
+
+// var session = {host: '63d5690765b903d98477c097', detail: {'63d5690765b903d98477c097': {name: 'stacey', tip: 2.42, bill: 12.32, is_paid: false}, '63d57d8704421e1d60c0dbbe': { name: "heather", tip: 1.75, bill: 10.21, is_paid: false}, '63d56921fb74d33c0a908e2b': {name: "Yui", tip: 1.75, bill: 10.21, is_paid: false}}, rest_name : 'Blue Sky', sub_total: 28.21, tip_total: 2.11, receipt: 'www.google.com'}
+// // db.User.updateOne({_id: 63d57d8704421e1d60c0dbbe}, { $addToSet: { "friends": ObjectId(63d5690765b903d98477c097)} })
+// var session = {host: '63d56a0483bd4d48f67c9981', detail: {'63d56a0483bd4d48f67c9981': {name: 'yuchen', tip: 1.47, bill: 21.41, is_paid: false}, '63d56921fb74d33c0a908e2b': {name: "Yui", tip: 1.75, bill: 10.21, is_paid: false}, '63d5690765b903d98477c097': {name: 'stacey', tip: 2.42, bill: 12.32, is_paid: false}}, rest_name : 'Red Sky', sub_total: 28.21, tip_total: 2.11, receipt: 'www.google.com'}
+// //var participants = [{
+// //    name: 'Jack Dorsey',
+// //    tip: 1.75,
+// //    bill: 24.2
+// //  },
+// //  {
+// //   name: "Bugs Bunny",
+// //   tip: 1.75,
+// //   bill: 10.21,
+// //  }
+// // ]
+
+// // var info = {
+// //   host: userId (this will be in a state variable, set after sign In)
+// //   rest_name: 'Chilis',
+// //   sub_total: 32.45,
+// //   tip: 34.5,
+// //   receipt: 'www.google.com'
+// // }
+
+// //expecting body to be {users, info} where users is an array of objects and
+// // info has the rest_name and other info
 console.log(req.body);
 
 var friends = [ ...req.body.friends];
@@ -43,19 +75,19 @@ details[req.body.host.user_id] = {
   is_paid: false
 }
 
-   db.Session.insertMany({
-    host: req.body.host.user_id,
-    detail: details,
-    rest_name: req.body.rest_name,
-    sub_total: req.body.sub_total,
-    tip_total: req.body.tip_total,
-    receipt: req.body.receipt,
-    active: true
-  })
-  // .then(sessionId => {
-  //   res.status(200).send('Session created!')
+  //  db.Session.insertMany({
+  //   host: req.body.host.user_id,
+  //   detail: details,
+  //   rest_name: req.body.rest_name,
+  //   sub_total: req.body.sub_total,
+  //   tip_total: req.body.tip_total,
+  //   receipt: req.body.receipt,
+  //   active: true
   // })
-    .then
+  helper.sendTexts(req.body)
+  .then(sessionId => {
+    res.status(200).send('Session created!')
+  })
   // .then((sessionId) => {
   //   console.log('sessionId', sessionId);
   //   return helper.sendTexts(req.body)
@@ -68,44 +100,44 @@ details[req.body.host.user_id] = {
   })
 }
 
-exports.updatePaymentStatus = function(req, res)  {
+exports.updatePaymentStatus = async function(req, res)  {
   const userId = req.body.userId;
   const comment = req.body.comment;
+  const data = req.body.data;
 
   if (!userId) {
-    res.status(500).send('User id not found');
+    return res.status(500).send('User id not found');
   } else {
-    let textBody = {};
-
-    db.Session.findOneAndUpdate({[`detail.${userId}`] : { $exists : true }}, {$set:{[`detail.${userId}.is_paid`]: true}})
-    .then(data => {
-      return db.Session.findById(sessionId)
-    })
-    .then(data => {
-      for (const friend in data.detail) {
-        if (!data.detail[friend].is_paid) {
-          res.status(200).send('Successfully updated payment status for the user');
-          return;
-        }
+    try {
+      let textBody = {};
+      let updatedSession = await db.Session.findOneAndUpdate(
+        {
+          _id: data.id,
+          [`detail.${userId}`] : { $exists: true }
+        },
+        { "$set": { [`detail.${userId}.is_paid`] : true }},
+        { new: true }
+      );
+      let entries = Object.entries(updatedSession.detail);
+      let paidUsers = entries.filter((x) => x[1].is_paid);
+      if (paidUsers.length !== entries.length) {
+        return res.status(200).send('Successfully updated payment status for the user');
+      } else {
+        let textBody = {};
+        let hostId = updatedSession.host.valueOf();
+        let hostData = await db.User.findById(hostId);
+        let hostPhoneNumber = hostData.phone_num;
+        let hostName = hostData.name;
+        textBody['host_name'] = hostName;
+        textBody['restaurant'] = updatedSession.rest_name;
+        textBody['sub_total'] = updatedSession.sub_total;
+        textBody['tip_total'] = updatedSession.tip_total;
+        helper.sendAllFriendHasPaidTexts(textBody);
+        res.status(200).send('Successfully updated payment status for the user');
       }
-      // all participants have paid
-      textBody['restaurant'] = data.rest_name;
-      textBody['sub_total'] = data.sub_total;
-      textBody['tip_total'] = data.tip_total;
-
-      const hostId = data.host.valueOf();
-      return db.User.findById(hostId)
-    })
-    .then(data => {
-      let hostPhoneNumber = data.phone_num;
-      let hostName = data.name;
-      textBody['host_name'] = hostName;
-      textBody['host_phone_number'] = hostPhoneNumber;
-      helper.sendAllFriendHasPaidTexts(textBody);
-      res.status(200).send('Successfully updated payment status for the user');
-    })
-    .catch((err) => {
-      res.status(500).send('Failed to update payment status for the user');
-    })
+    } catch (error) {
+      console.log(err);
+      res.status(500).send('Failed to update status')
+    }
   }
 }
