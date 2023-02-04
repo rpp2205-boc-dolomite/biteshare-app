8d07b7b6c75bbb5e5835b939f48f578c001e1a09
Zachary Atha <zacharyatha@gmail.com>
2023-02-04 13:21:25 -0500

    Started react testing library

--- client/src/components/Login.test.js
+++ client/src/components/Login.test.js
@@ -6,3 +6,16 @@ import SignIn from './Login.jsx';
 test('test runs', async () => {
     expect(1+1).toEqual(2)
 })
+
+test('Login page should render', async () => {
+    render(<SignIn />)
+    await waitFor(() => screen.getByRole('heading'))
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/SignIn/);
+})
+
+/*
+test('Login page should give error messages for incorrect username or password', async () => {
+    render(<SignIn />)
+})
+*/
