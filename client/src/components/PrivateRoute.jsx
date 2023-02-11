9b7e50e5ef75ddb6a27be74e0745743032c62690
Matt Alexander <dmalexander6@gmail.com>
2023-02-10 22:09:21 -0700

    add PrivateRoute component

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
