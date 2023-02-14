095659b05a8d5cd227c2c428570a863f3bc53aab
Matt Alexander <dmalexander6@gmail.com>
2023-02-14 16:39:43 -0700

    handleVerify fn

--- client/src/components/Forgot/VerifyPhone.jsx
+++ client/src/components/Forgot/VerifyPhone.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import {
   Typography,
   Avatar,
   Box,
   TextField,
   Button,
-  Alert
+  Alert,
+  Collapse
 } from '@mui/material';
 import {
   LockOutlinedIcon,
@@ -23,25 +25,69 @@ const startTimer = (sec, cb) => {
   }
 };
 
+
 export default function ({ setVerified }) {
   const [phoneNum, setPhoneNum] = useState(null);
   const [input, setInput] = useState('');
+  const [alertHidden, setAlertHidden] = useState(true);
+  const [timerId, setTimerId] = useState(null);
+
+  const doAlert = (text, severity, sec) => {
+    const alertElem = document.getElementById('vp-alert');
+    alertElem.severity = severity;
+    alertElem.innerHTML = text;
+
+    setAlertHidden(false);
+
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    if (sec) {
+      setTimerId(setTimeout(() => setAlertHidden(true), sec * 1000));
+    } else {
+      setTimerId(null);
+    }
+  };
 
   const handleVerify = () => {
+    axios.put('/api/verify', {phone_num: phoneNum, code: input});
+      .then(response => {
+        switch (response.status) {
+          case 201:
+            doAlert('Verification Succeeded!', 'success', 8);
+            setTimeout(() => setVerified(true), 2000);
+            break;
 
+          case 406:
+            break;
+        }
+      })
   }
 
   const handleSendCode = () => {
     const phone = parsePhoneNumber(input, 'US');
     if (phone) {
       setPhoneNum(phone.number);
-      const alert = document.getElementById('vp-alert');
-      const alertWrapper = document.getElementById('vp-alert-wrapper');
-      alert.severity = "info";
-      alert.innerHTML = "Thanks! Sending code now...";
-      alertWrapper.hidden = false;
-      setTimeout(() => alertWrapper.hidden = true, 8);
-
+      const inputElem = document.getElementById('vp-input');
+      inputElem.value = '';
+      doAlert("Sending code to: " + phone.formatNational(), 'info', 8);
+      axios.post('/api/verify', {phone_num: phone.number})
+        .then(response => {
+          if (response.status === 201) {
+            startTimer(60, sec => {
+              if (sec <= 10) {
+                console.log('SECONDS', sec);
+                inputElem.helperText = `${sec} second(s) remaining...`;
+              }
+              if (sec === 0) {
+                inputElem.helperText = '';
+              }
+            });
+          } else {
+            doAlert("There was an error! Please try again..", 'error', 8);
+          }
+          inputElem.focus();
+        });
     }
   }
 
@@ -60,7 +106,9 @@ export default function ({ setVerified }) {
         mb: 1
       }}
     >
-      <Box hidden id="vp-alert-wrapper"><Alert id="vp-alert" ></Alert></Box>
+      <Box component="span" display="inline" id="vp-alert-wrapper">
+        <Collapse in={!alertHidden} orientation="vertical"><Alert message={{display: 'inline'}} sx={{display: "inline"}} display="inline" id="vp-alert" /></Collapse>
+      </Box>
       {/* <Typography>Please confirm your phone number:</Typography> */}
       <TextField
         margin="normal"
