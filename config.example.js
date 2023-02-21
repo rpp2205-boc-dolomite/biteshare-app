7572a290c6f10667c12d38b4c4f031d1613bb161
Rain <pyc0422@gmail.com>
2023-02-21 11:13:45 -0600

    add config example file

---
+++ config.example.js
@@ -0,0 +1,18 @@
+
+PORT="Your Port"
+CORS_ORIGIN="Deployed URL"
+//accessing mongodb atlas
+DB_CLUSTER="Your mongoDb cluster"
+DB_NAME="Your database name"
+DB_USER="Your database user"
+DB_PASS="Your database password"
+//accessing twilio api
+TWILIO_BASE_URL="Twilio base URL"
+TWILIO_ACCOUNT_SID="Your twilio sid"
+TWILIO_AUTH_TOKEN="Your twilio auth token"
+TWILIO_PHONE_NUM="Your twilio phone number"
+//accessing yelp api
+YELP_CLIENT_ID="Your yelp client id"
+YELP_TOKEN="Your yelp token"
+//accessing jwt
+JWT_AUTH_SECRET_KEY="Your jwt form auth0 secret key"
