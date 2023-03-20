d4d248c850ee2f5089962d3da7f4ce61fe229b93
Matt Alexander <dmalexander6@gmail.com>
2023-03-20 12:09:34 -0600

    install spdy and enable https

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
+const spdy = require('spdy');
 const app = require('./app');
 
-app.listen(port, function () {
-  console.log(`BOC server running on port ${port}...`);
+const PORT = process.env.PORT || 3000;
+const useSSL = process.env.USE_SSL === 'true' ? true : false;
+
+const createServer = () => {
+  if (!useSSL) {
+    return app;
+
+  } else {
+    const CERT_DIR = path.resolve(__dirname, 'https');
+    const options = {
+      key: fs.readFileSync(path.join(CERT_DIR, process.env.SSL_KEY_FILE)),
+      cert: fs.readFileSync(path.join(CERT_DIR, process.env.SSL_CERT_FILE))
+    };
+
+    return spdy.createServer(options, app);
+  }
+};
+
+const server = createServer();
+
+server.listen(PORT, () => {
+  console.log(`BOC server listening on port ${PORT}`);
+  console.log(`HTTP/2 & SSL: ${useSSL ? 'enabled' : 'disabled'}`);
 });
